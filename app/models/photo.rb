b7857749ff72fac4fe4a040661f15df07ec1cde5
anrake <anrake@gmail.com>
2015-02-08 13:42:33 +0900

    add initial files for kokuin admin before generating a controller

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
 	belongs_to :imageable, polymorphic: true
-#	has_many :kokuin_matches
-#	has_many :kokuins, through: :kokuin_matches
+	has_many :kokuin_matches
+	has_many :kokuins, through: :kokuin_matches
 
 #	accepts_nested_attributes_for :kokuin_matches, :allow_destroy => true
 
